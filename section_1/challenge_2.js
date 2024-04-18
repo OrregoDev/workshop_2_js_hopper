44420fadc52e3b2f8fc2d10abb7fabff7aa6cd04
OrregoDev <camilo.orrego554@gmail.com>
2024-04-17 21:36:39 -0500

    WIP: advance challenge_2.js

---
+++ section_1/challenge_2.js
@@ -0,0 +1,59 @@
+// Types of Souvenirs
+
+const BUDGET = 10000;
+
+function getCost() {
+  while (true) {
+    const souvenirCost = prompt('What is the cost of the souvenir?');
+    if (!isNaN(souvenirCost) && souvenirCost > 0) {
+      return souvenirCost;
+    } else {
+      alert('Please enter a valid number.');
+    }
+  }
+}
+
+function askForSouvenir() {
+  const souvenirs = [];
+  while (true) {
+    const souvenirName = prompt('What is the name of the souvenir?');
+    const souvenirCost = getCost();
+    const souvenirAvailability = confirm('Is the souvenir available?');
+
+    const souvenir = {
+      name: souvenirName,
+      cost: souvenirCost,
+      availability: souvenirAvailability,
+    };
+
+    souvenirs.push(souvenir);
+
+    const anotherSouvenir = confirm('Do you want to add another souvenir?');
+    if (!anotherSouvenir) {
+      break;
+    }
+  }
+  return souvenirs;
+}
+
+function getAvailableSouvenir(souvenirs) {
+  const availableSouvenirs = [];
+  souvenirs.forEach((souvenir) => {
+    if (souvenir.availability === true) {
+      availableSouvenirs.push(souvenir);
+    }
+  });
+  return availableSouvenirs;
+}
+
+function showSouvenirs(availableSouvenirs) {
+  if (availableSouvenirs.length === 0) {
+    alert('No souvenirs available.');
+  } else {
+    console.log(availableSouvenirs);
+  }
+}
+
+const souvenirs = askForSouvenir();
+const availableSouvenirs = getAvailableSouvenir(souvenirs);
+showSouvenirs(availableSouvenirs);
