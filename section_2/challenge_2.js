356da46e021277da38acc5671d606df7c536f70a
OrregoDev <camilo.orrego554@gmail.com>
2024-04-19 20:08:11 -0500

    WIP advance in challenge_2.js

--- section_2/challenge_2.js
+++ section_2/challenge_2.js
@@ -1,1 +1,25 @@
 // personal budget calculator
+
+function getAmount(promptMessage) {
+  while (true) {
+    const amount = Number(prompt(promptMessage));
+    if (!Number.isNaN(amount) && amount > 0) {
+      return amount;
+    } else {
+      alert('Please enter a valid number.');
+    }
+  }
+}
+
+const dailyBudget = getAmount('How much is your daily budget?');
+const dailySavingsGoal = getAmount('How much is your minimum daily savings?');
+const eatingOutPrice = getAmount('How much does your eating out cost?');
+const bookPrice = getAmount('How much does your book cost?');
+
+if (dailyBudget <= dailySavingsGoal) {
+  alert('Your daily budget is not enough. You should adjust your budget.');
+} else if (dailyBudget - dailySavingsGoal >= eatingOutPrice) {
+  alert('You could go out to eat');
+} else if (dailyBudget - dailySavingsGoal >= bookPrice) {
+  alert('You could buy a book.');
+}
