3b88397d49fa88947624a7203c2b6e9bac0676f3
OrregoDev <camilo.orrego554@gmail.com>
2024-04-21 20:46:52 -0500

    finish section_2/challenge_3

--- section_2/challenge_3.js
+++ section_2/challenge_3.js
@@ -1,7 +1,7 @@
 // Last minute decisions
 
-const paraguas = { name: 'Umbrella', weight: 1, essential: true };
-const cámara = { name: 'Camera', weight: 2, essential: false };
+const umbrella = { name: 'Umbrella', weight: 1, essential: true };
+const camera = { name: 'Camera', weight: 2, essential: false };
 const hat = { name: 'hat', weight: 1, essential: false };
 const bottle = { name: 'bottle', weight: 1, essential: true };
 
@@ -16,20 +16,22 @@ function getAmount(promptMessage) {
   }
 }
 
+let weatherForecast;
 while (true) {
-  const weathers = ['sunny', 'cloudy', 'rainy'];
-  const weatherForecast = prompt(
-    'What is the weather forecast? sunny, cloudy or rainy?'
-  );
-  if (weathers.indexOf(weatherForecast)) {
+  weatherForecast = prompt('What is the weather forecast? sunny or rainy?');
+  if (
+    weatherForecast.toLowerCase() !== 'sunny' &&
+    weatherForecast.toLowerCase() !== 'rainy'
+  ) {
     alert('Please enter a valid weather forecast');
   } else {
     break;
   }
 }
 
+let weightLimit;
 while (true) {
-  const weightLimit = getAmount('How much weight (in kg) can you carry?');
+  weightLimit = getAmount('How much weight (in kg) can you carry?');
   if (weightLimit > 0) {
     break;
   } else {
@@ -37,12 +39,9 @@ while (true) {
   }
 }
 
-const tripDuration = getAmount('How long is your trip (in days)?');
-
+let remainingLuggageSpace;
 while (true) {
-  const remainingLuggageSpace = getAmount(
-    'How much space (in kg) do you have?'
-  );
+  remainingLuggageSpace = getAmount('How much space (in kg) do you have?');
   if (remainingLuggageSpace > 0) {
     break;
   } else {
@@ -50,4 +49,34 @@ while (true) {
   }
 }
 
-function suggestedArticles(remainingLuggageSpace, weightLimit, tripDuration) {}
+function getitems(weatherForecast, remainingLuggageSpace, weightLimit) {
+  let suggesteditems = [];
+  if (weatherForecast === 'rainy') {
+    suggesteditems.push(umbrella);
+  }
+
+  if (remainingLuggageSpace >= hat.weight && weightLimit >= hat.weight) {
+    suggesteditems.push(hat);
+  }
+
+  if (remainingLuggageSpace >= camera.weight && weightLimit >= camera.weight) {
+    suggesteditems.push(camera);
+  }
+
+  if (remainingLuggageSpace >= bottle.weight && weightLimit >= bottle.weight) {
+    suggesteditems.push(bottle);
+  }
+
+  if (suggesteditems.length > 0) {
+    alert(
+      'Based on the given conditions, it is recommended to carry the following items:'
+    );
+    suggesteditems.forEach((item) => {
+      alert(`- ${item.name} (weight: ${item.weight} kg)`);
+    });
+  } else {
+    alert('There are no recommended items based on the given conditions.');
+  }
+}
+
+getitems(weatherForecast, remainingLuggageSpace, weightLimit);
